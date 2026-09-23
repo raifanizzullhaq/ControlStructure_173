d2838e31d23ac55e4c97d251024d03ec648e6caa
raifanizzullhaq <raifanizzulhaq3>
2026-09-23 16:09:55 +0700

    Add initial implementation of grade evaluation program

---
+++ prak1.py
@@ -0,0 +1,13 @@
+grade=int(input("masukkan grade:")
+
+if grade >=90:
+    print ("Excellent performance")
+elif grade >=80:
+    print ("Very good performance")
+elif grade >=70:
+    print ("good performance")
+elif grade >=60:
+    print ("Average performance")
+else:
+    print ("Poor performance")
+    
